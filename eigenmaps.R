8e4fa03523ebabc728461db2d1489b6cfdb3a9ce
Yanran Li <lyr1027@gmail.com>
2015-06-23 16:19:39 +0800

    Update eigenmaps.R

--- eigenmaps.R
+++ eigenmaps.R
@@ -1,7 +1,7 @@
 seq1 <- c(1,2,3,2,1,2,3,2,1)
 mat1 <- matrix(seq1, 3)
-seq2 <- rep(0, 9)
-mat2 <- matrix(seq2, 3)
+seq2 <- rep(0, 9) 
+mat2 <- matrix(seq2, 3) # regularization via mat2 and mat3, version 0 only zero, indicating orthogological relations
 mat12 <- cbind(mat1, mat2)
 mat34 <- cbind(mat2, mat1)
 W <- rbind(mat12, mat34)
