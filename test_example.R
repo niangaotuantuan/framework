ff4f4e396391ee0666a379f4b6c773b5e29a5f07
Yanran Li <lyr1027@gmail.com>
2015-08-27 17:38:03 +0800

    Update test_example.R

--- test_example.R
+++ test_example.R
@@ -1,11 +1,19 @@
-seq1 <- c(1,2,3,2,1,2,3,2,1)
-mat1 <- matrix(seq1, 3)
-seq2 <- rep(0, 9) 
-mat2 <- matrix(seq2, 3) # regularization via mat2 and mat3, version 0 only zero, indicating orthogological relations
-mat12 <- cbind(mat1, mat2)
-mat34 <- cbind(mat2, mat1)
-W <- rbind(mat12, mat34)
+#input
+m <- 100
+n <- 30
+
+W1 <- diag(m)
+W1[lower.tri(W1)] <- rnorm(sum(1:m-1))
+W1[upper.tri(W1)] <- t(W1)[upper.tri(t(W1))] #symmetric matrix
+
+W2 <- diag(n)
+W2[lower.tri(W2)] <- rnorm(sum(1:n-1))
+W2[upper.tri(W2)] <- t(W2)[upper.tri(t(W2))] #symmetric matrix
+
+W12 <- matrix(data=rnorm(m*n),nrow=n,ncol=m)
+
+W <- rbind(cbind(W1,t(W12)),cbind(W12,W2))
 cols <- colSums(W)
-D <- diag(x=cols, nrow=6, ncol=6)
+D <- diag(x=cols, nrow=m+n, ncol=m+n)
 L <- D-W
 eigens <- eigen(solve(D,L)) # generalized eigenvector problem
