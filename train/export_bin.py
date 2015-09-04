ffb34299259cf47fbf273fa566be3d89826a23fb
Yanran Li <lyr1027@gmail.com>
2015-09-04 08:55:22 +0800

    Update export_bin.py

--- train/export_bin.py
+++ train/export_bin.py
@@ -1,5 +1,5 @@
-import codecs
 from gensim.models import Word2Vec
+import codecs
 
 def main():
     path_to_model = 'GoogleNews-vectors-negative300.bin'
@@ -13,18 +13,14 @@ def export_to_file(path_to_model, output_file):
     print('done loading Word2Vec')
     vocab = model.vocab
     for mid in vocab:
-        #print(model[mid])
-        #print(mid)
         vector = list()
         for dimension in model[mid]:
             vector.append(str(dimension))
         #line = { "mid": mid, "vector": vector  }
         vector_str = ",".join(vector)
         line = mid + "\t"  + vector_str
-        #line = json.dumps(line)
         output.write(line + "\n")
     output.close()
 
 if __name__ == "__main__":
     main()
-    #cProfile.run('main()') # if you want to do some profiling
